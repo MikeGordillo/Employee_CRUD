340969e9fbc464c011c78c0c15bcda463e21fb08
Mike Gordillo <mikegordillotattoo@gmail.com>
2021-05-22 16:02:35 -0400

    not sure how to link the database or if thats even my issue deploying to heroku

--- employeeTracker/connection/connection.js
+++ employeeTracker/connection/connection.js
@@ -3,7 +3,7 @@ const mysql2 = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql2.createConnection({
-    user: process.env.JAWS_DB,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: 'localhost',
