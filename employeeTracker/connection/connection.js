c7f323e3ca282de9d79f884fa210c060a09cd1f9
Mike Gordillo <mikegordillotattoo@gmail.com>
2021-05-22 15:58:39 -0400

    jawsdb

--- employeeTracker/connection/connection.js
+++ employeeTracker/connection/connection.js
@@ -3,7 +3,7 @@ const mysql2 = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql2.createConnection({
-    user: process.env.DB_USER,
+    user: process.env.JAWS_DB,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: 'localhost',
